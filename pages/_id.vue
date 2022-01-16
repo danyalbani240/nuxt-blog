49eec610c8c72751d63ea9f3b42f81eb7da2f7ee
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-16 10:56:18 +0330

    user id created

---
+++ pages/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      hi this is user {{$route.params.id}}
+  </div>
+</template>
+
+<script>
+export default {
+    validate({params}){
+        
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
+
+<style>
+
+</style>
