b5a1274558ad82a6122b3eb09d7ccd15f58cddd1
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-16 11:14:10 +0330

    making it nested

--- pages/users.vue
+++ pages/users.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>hi</div>
+  <div>
+    <div class="flex">
+      <input type="text" placeholder="enter user id" v-model="userId" />
+      <button @click="goTo">click</button>
+    </div>
+    <nuxt-child />
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+  methods: {
+    goTo() {
+      if(this.$route.name == 'users-id'){
+          this.$router.push(this.userId)
+      }else{
+          this.$router.push(`users/${this.userId}`)
+      }
+    },
+  },
+}
+</script>
