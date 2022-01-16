92ff11a9fa90f12d6405e0dc6eead16459fdae02
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-16 10:43:46 +0330

    default layout with nav created

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-2">
+      <nav class="flex justfy-between bg-gray-100">
+          <nuxt-link to="/">Home</nuxt-link>
+          <nuxt-link to="/users">Users</nuxt-link>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
