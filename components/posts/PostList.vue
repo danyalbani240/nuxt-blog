55c44e26019536765bd4c3627ad10ea2c48cdf59
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-19 02:21:20 +0330

    Post List Component Created

---
+++ components/posts/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post-list pt-32 flex justify-evenly flex-wrap">
+    <PostPreview
+      thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg"
+      title="post-title"
+      content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    /><PostPreview
+      thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg"
+      title="post-titl2"
+      content="lorem1 ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    /><PostPreview
+      thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg"
+      title="post-title3"
+      content="lorem2 ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    />
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/posts/PostPreview.vue'
+export default {
+    components:{
+        PostPreview
+    }
+}
+</script>
+
+<style>
+/* .post-list {
+} */
+</style>
