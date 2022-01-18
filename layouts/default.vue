75137fe71ccc7e172a0667be9e429d8d9af27660
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-18 17:24:17 +0330

    preparing for hte blog

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="p-2">
-      <nav class="flex  h-10 items-center justify-between bg-gray-100">
-          <nuxt-link class="flex-1 text-center" to="/">Home</nuxt-link>
-          <nuxt-link class="flex-1 text-center" to="/users">Users</nuxt-link>
-      </nav>
-      <nuxt />
-  </div>
+  <nuxt />
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
