e51ed3e8c8fe71742b80e70ae7c74375caa1feb7
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-23 21:27:15 +0330

    Base Components Created

---
+++ components/Base/AppButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="base-button" @click="$emit('click', $event)">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ components/Base/AppInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+    type="text"
+    class="base-input"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
