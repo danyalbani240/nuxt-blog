71bdf9e67a8854c097339bc9c578a313d5912db5
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-24 23:36:48 +0330

    post edit form added

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="post-edit"></div>
+  <div class="post-edit">
+    <form @submit.prevent="">
+      <label>Title:</label>
+      <app-input v-model="title" placeholder="What is Javascript,...." />
+      <label>content:</label>
+      <app-input
+        v-model="content"
+        :textArea="true"
+        placeholder="javscript is ....."
+      />
+      <app-button @click="formSubmit">Submit</app-button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      title: '',
+      content: '',
+    }
+  },
+  components: {
+    AppButton,
+    AppInput,
+  },
+  methods: {
+    formSubmit() {
+      console.log(this.title, this.content)
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
