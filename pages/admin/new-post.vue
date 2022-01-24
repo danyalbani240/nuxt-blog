fb9fbe497baf5abe523dae3252dc60df4739ca71
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-24 23:31:33 +0330

    new post is working

--- pages/admin/new-post.vue
+++ pages/admin/new-post.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="new-post">
-      <form @submit.prevent=""></form>
+    <form @submit.prevent="">
+      <label>Title:</label>
+      <app-input v-model="title" placeholder="What is Javascript,...." />
+      <label>content:</label>
+      <app-input
+        v-model="content"
+        :textArea="true"
+        placeholder="javscript is ....."
+      />
+      <app-button @click="formSubmit">Submit</app-button>
+    </form>
   </div>
 </template>
 
 <script>
+import AppButton from '~/components/Base/AppButton.vue'
+import AppInput from '~/components/Base/AppInput.vue'
 export default {
-
+  data() {
+    return {
+      title: '',
+      content: '',
+    }
+  },
+  components: {
+    AppButton,
+    AppInput,
+  },
+  methods: {
+    formSubmit() {
+      console.log(this.title, this.content)
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
