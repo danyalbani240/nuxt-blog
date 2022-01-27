6bc61d1e4d0e30a2224dcd13cc9db82b1c0c5000
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-27 11:32:16 +0330

    make the async Data with Promise This is The Correct Way for async Data

--- pages/index.vue
+++ pages/index.vue
@@ -15,23 +15,41 @@ export default {
   //     loadedPosts: [],
   //   }
   // },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: 1,
-            title: 'post-1',
-            content: 'post one content here',
-          },
-          {
-            id: 2,
-            title: 'post-2',
-            content: 'post two content here',
-          },
-        ],
-      })
-    }, 1500)
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: 1,
+              title: 'post-1',
+              content: 'post one content here',
+            },
+            {
+              id: 2,
+              title: 'post-2',
+              content: 'post two content here',
+            },
+          ],
+        })
+      }, 1500)
+    })
+    // setTimeout(() => {
+    //   callback(null, {
+    //     loadedPosts: [
+    //       {
+    //         id: 1,
+    //         title: 'post-1',
+    //         content: 'post one content here',
+    //       },
+    //       {
+    //         id: 2,
+    //         title: 'post-2',
+    //         content: 'post two content here',
+    //       },
+    //     ],
+    //   })
+    // }, 1500)
   },
 }
 </script>
