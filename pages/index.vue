570d751e4afeef175bd169c7fae8c6b65fb28a02
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-27 09:51:11 +0330

    making popst list allright

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main-page">
-    <PostList thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg" title="post-title" content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum " />
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostList from '@/components/posts/PostList.vue';
+import PostList from '@/components/posts/PostList.vue'
 export default {
   components: {
     PostList,
