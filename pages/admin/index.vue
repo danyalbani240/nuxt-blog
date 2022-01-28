5ee70a5d52c1c89d8f8554ae3189920f61dac275
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-28 11:33:53 +0330

    new post link added to admin  index page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="admin-page">
+    <app-button @click="$router.push('/admin/new-post')"
+      >Create Post</app-button
+    >
+
     <PostList :admin="true" />
   </div>
 </template>
 
 <script>
 import PostList from '~/components/posts/PostList.vue'
+import AppButton from '~/components/Base/AppButton.vue'
 export default {
-  components: { PostList },
-
+  components: { PostList, AppButton },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
