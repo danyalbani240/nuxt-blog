0f801459677f6f4c8c483b202853a8581248bb4a
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-09 19:52:38 +0330

    inputs and buttons are setted

--- components/SignUp.vue
+++ components/SignUp.vue
@@ -1,7 +1,38 @@
-<template></template>
+<template>
+  <form @submit.prevent="handleSignUp">
+    <BaseInput placeholder="username" />
+    <BaseInput
+      placeholder="example@email.com"
+      :type="'email'"
+      :value="userData.email"
+    />
+    <BaseInput
+      :value="userData.password"
+      :type="'password'"
+      placeholder="password"
+    />
+    <BaseButton> Submit </BaseButton>
+  </form>
+</template>
 
 <script>
-export default {}
+import BaseInput from '~/components/Base/AppInput.vue'
+import BaseButton from '~/components/Base/AppButton.vue'
+export default {
+  components: {
+    BaseButton,
+    BaseInput,
+  },
+  data() {
+    return {
+      userData: {
+        username: '',
+        email: '',
+        password: '',
+      },
+    }
+  },
+}
 </script>
 
 <style></style>
