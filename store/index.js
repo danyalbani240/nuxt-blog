11e6da418a3de4cbcd0d67442c19913b308da773
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-12 09:30:13 +0330

    setToken mutation added,edit post action added

--- store/index.js
+++ store/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 export const state = () => ({
   loadedPosts: [],
+  token: null,
 })
 export const mutations = {
   setPosts(state, posts) {
     state.loadedPosts = posts
+  },
+  setToken(state, token) {
+    state.token = token
   },
 }
 export const actions = {
@@ -28,9 +32,39 @@ export const actions = {
   setPosts(context, posts) {
     context.commit('setPosts', posts)
   },
+  editPost(vueXcontext, newDAta) {
+    axios({
+      method: 'put',
+      url: 'https://nuxt-bc2d9-default-rtdb.firebaseio.com/posts/',
+    })
+  },
+  signUp(vueContext, userData) {
+    axios({
+      method: 'post',
+      url:
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        process.env.apiKey,
+      data: { ...userData, returnSecureToken: true },
+    })
+  },
+  signIn(vueContext, userData) {
+    return axios({
+      method: 'post',
+      url:
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        process.env.apiKey,
+      data: { ...userData, returnSecureToken: true },
+    }).then((res) => {
+      vueContext.commit('setToken', res.data.idToken)
+      return true
+    })
+  },
 }
 export const getters = {
   loadedPosts(state) {
     return state.loadedPosts
+  },
+  token(state) {
+    return state.token
   },
 }
