92978ae87d64758d4841276f6ae78159a5f2a318
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-17 14:47:42 +0330

    hover added

--- components/Base/AppButton.vue
+++ components/Base/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="base-button px-4 py-2 bg-blue-200 text-gray-600 rounded"
+    class="base-button px-4 py-2 bg-blue-200 text-gray-600 rounded hover:text-gray-800 hover:bg-blue-300 transition-all"
     @click="$emit('click', $event)"
   >
     <slot></slot>
