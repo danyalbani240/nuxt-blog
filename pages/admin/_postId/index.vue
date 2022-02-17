495defa7f7b3cde09b20cd29a9374d9a95c0e402
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-17 11:54:02 +0330

    fetching post data from firebase

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -7,7 +7,7 @@
       <app-input
         v-model="content"
         :textArea="true"
-        placeholder="javscript is ....."
+        placeholder="javascript is ....."
       />
       <app-button @click="formSubmit">Submit</app-button>
     </form>
@@ -15,14 +15,35 @@
 </template>
 
 <script>
-import AppButton from '@/components/Base/AppButton.vue';
-import AppInput from '@/components/Base/AppInput.vue';
+import AppButton from '@/components/Base/AppButton.vue'
+import AppInput from '@/components/Base/AppInput.vue'
+import axios from 'axios'
+
 export default {
+  async fetch(context) {
+    this.postBaseData = await axios
+      .get(
+        `https://nuxt-bc2d9-default-rtdb.firebaseio.com/posts/${context.params.postId}.json`
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        console.log(err)
+      })
+  },
   data() {
     return {
+      postBaseData: {},
       title: '',
       content: '',
     }
+  },
+  watch: {
+    postBaseData: (newValue) => {
+      console.log(newValue)
+    },
+  },
+  mounted() {
+    console.log(this.postBaseData)
   },
   components: {
     AppButton,
