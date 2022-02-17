564c179002c58565dcc49b648d2491ac125f7f47
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-17 14:54:10 +0330

    edit post form added to page

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,58 +1,24 @@
 <template>
-  <div class="post-edit">
-    <form @submit.prevent="">
-      <label>Title:</label>
-      <app-input v-model="title" placeholder="What is Javascript,...." />
-      <label>content:</label>
-      <app-input
-        v-model="content"
-        :textArea="true"
-        placeholder="javascript is ....."
-      />
-      <app-button @click="formSubmit">Submit</app-button>
-    </form>
-  </div>
+  <BaseForm @submit="handleSubmit" :editPostForm="true" />
 </template>
 
 <script>
 import AppButton from '@/components/Base/AppButton.vue'
 import AppInput from '@/components/Base/AppInput.vue'
-import axios from 'axios'
+import BaseForm from '~/components/Base/BaseForm.vue'
 
 export default {
-  async fetch(context) {
-    this.postBaseData = await axios
-      .get(
-        `https://nuxt-bc2d9-default-rtdb.firebaseio.com/posts/${context.params.postId}.json`
-      )
-      .then((res) => res.data)
-      .catch((err) => {
-        console.log(err)
-      })
-  },
   data() {
-    return {
-      postBaseData: {},
-      title: '',
-      content: '',
-    }
+    return {}
   },
-  watch: {
-    postBaseData: (newValue) => {
-      console.log(newValue)
-    },
-  },
-  mounted() {
-    console.log(this.postBaseData)
-  },
+
   components: {
     AppButton,
     AppInput,
+    BaseForm,
   },
   methods: {
-    formSubmit() {
-      console.log(this.title, this.content)
-    },
+    handleSubmit() {},
   },
 }
 </script>
