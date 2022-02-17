8f39ddf24fffc56dbe94488a6174997bdd9ee934
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-17 14:47:57 +0330

    label added

--- components/Base/AppInput.vue
+++ components/Base/AppInput.vue
@@ -1,22 +1,34 @@
 <template>
-  <input
-    :type="type"
-    class="base-input py-2 pl-4 focus:outline-none focus:border-gray-500 focus:border-4 border-2 rounded pr-10"
-    :value="value"
-    @input="$emit('input', $event.target.value)"
-    v-if="!textArea"
-    :placeholder="placeholder"
-  />
-  <textarea
-    v-else
-    :value="value"
-    @input="$emit('input', $event.target.value)"
-  />
+  <div v-if="!textArea" class="flex flex-col mx-auto">
+    <label>{{ label }}:</label>
+
+    <input
+      :type="type"
+      class="base-input py-2 pl-4 focus:outline-none focus:border-gray-500 focus:border-4 border-2 rounded pr-10 mt-2 transition-all"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="placeholder"
+    />
+  </div>
+
+  <div v-else class="flex flex-col mx-auto">
+    <label>{{ label }}:</label>
+    <textarea
+      class="y-2 pl-4 focus:outline-none focus:border-gray-500 focus:border-4 border-2 rounded pr-10 h-40 pt-2 mt-2 transition-all"
+      :value="value"
+      :placeholder="placeholder"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   props: {
+    label: {
+      type: String,
+      required: false,
+    },
     placeholder: {
       type: String,
       required: false,
