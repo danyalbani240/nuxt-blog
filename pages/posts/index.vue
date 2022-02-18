b34f082449724c7900ec595ee5b2c453a31bd072
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-18 11:05:27 +0330

    laode more aded to posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="posts-page">
     <PostList :posts="loadedPosts" />
+    <div class="flex items-center mt-20">
+      <AppButton class="mx-auto" @click="loadMore">LoadMore</AppButton>
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from '~/components/posts/PostList.vue'
+import AppButton from '~/components/Base/AppButton.vue'
 export default {
   components: {
     PostList,
+    AppButton,
   },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
+    },
+  },
+  methods: {
+    loadMore() {
+      this.$store.dispatch('loadNewPosts')
     },
   },
 }
