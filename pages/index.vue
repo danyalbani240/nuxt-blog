38058295f45b78dff06e57992ff671ec912183c5
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-18 11:04:16 +0330

    load more added

--- pages/index.vue
+++ pages/index.vue
@@ -2,81 +2,29 @@
   <div class="main-page">
     <div v-if="!loadedPosts">Loading...</div>
     <PostList v-else :posts="loadedPosts" />
+    <div class="flex items-center mt-20">
+      <AppButton class="mx-auto" @click="loadMore">LoadMore</AppButton>
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from '@/components/posts/PostList.vue'
+import AppButton from '~/components/Base/AppButton.vue'
 export default {
   components: {
     PostList,
+    AppButton,
   },
-  // data() {
-  //   return {
-  //     loadedPosts: [],
-  //   }
-  // },
-  // async fetch(context) {
 
-  //   if (context.store.getters.loadedPosts == 0) {
-  //     console.log(context)
-
-  //     return new Promise((resolve, reject) => {
-  //       setTimeout(() => {
-  //         resolve({
-  //           loadedPosts: [
-  //             {
-  //               id: 1,
-  //               title: 'post-1',
-  //               content: 'post one content here',
-  //             },
-  //             {
-  //               id: 2,
-  //               title: 'post-2',
-  //               content: 'post two content here',
-  //             },
-  //           ],
-  //         })
-  //       }, 2000)
-  //     }).then((data) => context.store.commit('setPosts', data.loadedPosts))
-  //   }
-
-  //   // setTimeout(() => {
-  //   //   callback(null, {
-  //   //     loadedPosts: [
-  //   //       {
-  //   //         id: 1,
-  //   //         title: 'post-1',
-  //   //         content: 'post one content here',
-  //   //       },
-  //   //       {
-  //   //         id: 2,
-  //   //         title: 'post-2',
-  //   //         content: 'post two content here',
-  //   //       },
-  //   //     ],
-  //   //   })
-  //   // }, 1500)
-  //   // this.loadedPosts = await new Promise((resolve, reject) => {
-  //   //   setTimeout(() => {
-  //   //     resolve([
-  //   //       {
-  //   //         id: 1,
-  //   //         title: 'post-1',
-  //   //         content: 'post one content here',
-  //   //       },
-  //   //       {
-  //   //         id: 2,
-  //   //         title: 'post-2',
-  //   //         content: 'post two content here',
-  //   //       },
-  //   //     ])
-  //   //   }, 15000)
-  //   // })
-  // },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
+    },
+  },
+  methods: {
+    loadMore() {
+      this.$store.dispatch('loadNewPosts')
     },
   },
 }
