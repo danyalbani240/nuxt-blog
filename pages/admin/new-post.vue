ed699c3ffec605da0d2ebd88c5955ba9811c5e20
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-18 18:54:37 +0330

    creating new post

--- pages/admin/new-post.vue
+++ pages/admin/new-post.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="new-post">
-    <BaseForm />
+    <BaseForm @submit="formSubmitHandle" />
   </div>
 </template>
 
@@ -11,14 +11,7 @@ import BaseForm from '~/components/Base/BaseForm.vue'
 import axios from 'axios'
 export default {
   data() {
-    return {
-      post: {
-        title: '',
-        content: '',
-        date: Date.now(),
-        author: 'Danyal',
-      },
-    }
+    return {}
   },
   components: {
     AppButton,
@@ -26,19 +19,14 @@ export default {
     BaseForm,
   },
   methods: {
-    formSubmit() {
-      console.log(this.post)
-      axios
-        .post(
-          'https://nuxt-bc2d9-default-rtdb.firebaseio.com/posts.json',
-          this.post
-        )
-        .then((res) => {
-          console.log(res)
-          if (res.status == 200) {
-            this.$router.push('/')
-          }
+    formSubmitHandle(post) {
+      this.$store
+        .dispatch('createPost', post)
+        .then((result) => {
+          console.log(result)
+          this.$router.push('/')
         })
+        .catch((err) => {})
     },
   },
 }
