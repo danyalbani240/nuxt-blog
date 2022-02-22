9e5fad29323e3bd124234e3aa410f2475fe85156
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-22 10:49:29 +0330

    changing background image and color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
+  <div class="layout-container min-h-screen flex flex-col bg-no-repeat">
     <Navigation />
     <nuxt />
   </div>
 </template>
 
 <script>
-import Navigation from "../components/Navigation";
+import Navigation from '../components/Navigation'
 export default {
-  components:{
-    Navigation
-  }
+  components: {
+    Navigation,
+  },
 }
 </script>
 
-<style></style>
+<style>
+.layout-container {
+  background-color: #e3e6ec;
+  width: 100%;
+  background-image: url(../assets/images/bgcircle.svg);
+  background-position: top center;
+}
+input:autofill {
+  background: #fff; /* or any other */
+}
+</style>
