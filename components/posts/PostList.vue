882cb89802097119145fc23e1fa3073e604a6562
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-22 17:56:20 +0330

    post list style changed and post preview first post now is bigger

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="post-list pt-32 flex justify-evenly flex-wrap">
+  <div class="post-list w-3/4 my-20 mx-auto flex justify-between flex-wrap">
     <PostPreview
       :admin="admin"
       thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg"
@@ -33,5 +33,6 @@ export default {
 
 <style>
 /* .post-list {
+
 } */
 </style>
