24f3f1001b4f1bb47bb48c57261253f46182df9f
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-22 17:58:00 +0330

    h1 style changed in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main-page flex-1 flex flex-col justify-between">
+    <h1>Danyal Blog</h1>
     <div v-if="!loadedPosts">Loading...</div>
     <PostList v-else :posts="loadedPosts" />
     <div class="flex justify-center mb-2">
@@ -30,4 +31,14 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+h1 {
+  text-align: center;
+  font-size: 200px;
+  font-weight: bold;
+  border-bottom: 3px solid black;
+  display: inline-block;
+  width: 75%;
+  margin: 0 auto;
+}
+</style>
