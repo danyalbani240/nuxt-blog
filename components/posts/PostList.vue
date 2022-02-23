5e419ef00197d64088b4ce6a4a173faf2efaafdb
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-23 09:37:21 +0330

    thumbnail binded post list

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -2,7 +2,7 @@
   <div class="post-list w-3/4 my-20 mx-auto flex justify-between flex-wrap">
     <PostPreview
       :admin="admin"
-      thumbnail="https://mlmuuous6dwe.i.optimole.com/DfwWcdQ-JK5kWYU6/w:auto/h:auto/q:auto/https://technation.io/wp-content/uploads/2017/11/91F5041A-B5D4-4648-AA55-C39A0DBBAAC3.jpeg"
+      :thumbnail="post.thumbnail"
       :title="post.title"
       :content="post.content"
       :id="post.id"
